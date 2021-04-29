c53223e97650e5c048bdf608dc13dbcd1bdb534f
niker <niker@seznam.cz>
2021-04-29 17:18:15 +0200

    Add files via upload

---
+++ elitecore/elitecore-async.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+(function()
+{
+  /**
+   * Wait for a delay asynchronously.
+   * @param delay {int}
+   * @return {Promise}
+   */
+  Promise.Delay = (delay) =>
+  {
+    return new Promise(resolve => {
+      setTimeout(() => { resolve(''); }, delay);
+    });
+  };
+
+})();
